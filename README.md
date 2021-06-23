05efeddaf7fffbfcfce88b0f6735f76b496a927a
Jose Moreno <jose.moreno@microsoft.com>
2021-06-23 17:14:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,142 @@
+# Convert test to Azure code
+
+The purpose of this repo is providing an example that illustrates a possibility to convert configuration files from disparate sources to code that can be imported into Azure.
+
+As source configuration the example in this repo uses a configuration extracted from a Fortigate firewall. The goal of this repo is not focusing on converting Fortigate configuration to Azure (other repos out there already extract Fortigate config to JSON, which would be easy to parse), but to offering a generic schema for processing generic, text-based configurations.
+
+The Python code in the example will process the source file line by line, and through an extensive usage of regex will create an array of objects. This array can be converted to either an ARM template or to Azure CLI code (which allows to test object after object).
+
+For example, let's say that the source configuration is a firewall ruleset with two policies:
+
+```
+# -- policy 1001 --
+action: accept
+dstaddr:
+192.168.20.15/32
+service:
+tcp 1433
+srcaddr:
+172.17.1.100 - 172.17.1.200
+172.17.2.100 - 172.17.2.200
+172.17.3.100 - 172.17.3.200
+172.17.4.100 - 172.17.4.200
+# -- policy 1002 --
+dstaddr:
+0.0.0.0/0
+service:
+protocol IP
+srcaddr:
+172.16.1.0/24
+172.16.2.0/24
+```
+
+The Python script can be run without any flags, and it will default to generate Azure CLI commands to replicate those two policies into an Azure Firewall Policy resource:
+
+```
+(base) C:\Repos\azure-config-converter>python .\read_fortigate_config.py --file fortigate_test.txt
+az group create -n fortinet -l westeurope
+az network firewall policy create -n mypolicy -g fortinet
+az network firewall policy rule-collection-group create -n fortinet --policy-name mypolicy -g fortinet --priority 1000
+az network firewall policy rule-collection-group collection add-filter-collection --rule-type NetworkRule -o none -g fortinet --rcg-name fortinet --policy-name mypolicy --action  --collection-priority 1010 --name 1001-sx6j --rule-name 1001-sx6j --source-addresses 172.17.1.100-172.17.1.200 172.17.2.100-172.17.2.200 172.17.3.100-172.17.3.200 172.17.4.100-172.17.4.200 --destination-addresses 192.168.20.15/32 --ip-protocols tcp --destination-ports 1433
+az network firewall policy rule-collection-group collection add-filter-collection --rule-type NetworkRule -o none -g fortinet --rcg-name fortinet --policy-name mypolicy --action  --collection-priority 1020 --name 1002-UWxo --rule-name 1002-UWxo --source-addresses 172.16.1.0/24 172.16.2.0/24 --destination-addresses 0.0.0.0/0 --ip-protocols Any --destination-ports '*'
+```
+
+Or if an ARM template is preferred, the `--format` flag can be used:
+
+```
+(base) C:\Repos\azure-config-converter>python .\read_fortigate_config.py --file fortigate_test.txt --format json
+{
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": { },
+    "variables": {
+        "location": "[resourceGroup().location]"
+    },
+    "resources": [
+        {
+            "type": "Microsoft.Network/firewallPolicies",
+            "apiVersion": "2020-11-01",
+            "name": "jsonpolicy",
+            "location": "[variables('location')]",
+            "properties": {
+                "sku": {
+                    "tier": "Standard"
+                },
+                "threatIntelMode": "Alert"
+            }
+        },
+        {
+            "type": "Microsoft.Network/firewallPolicies/ruleCollectionGroups",
+            "apiVersion": "2020-11-01",
+            "name": "jsonpolicy/fortinet",
+            "location": "[variables('location')]",
+            "dependsOn": [
+                "[resourceId('Microsoft.Network/firewallPolicies', 'jsonpolicy'))]"
+            ],
+            "properties": {
+                "priority": 1000,
+                "ruleCollections": [
+{
+                        "ruleCollectionType": "FirewallPolicyFilterRuleCollection",
+                        "action": {
+                            "type": "Allow"
+                        },
+                        "rules": [
+                            {
+                                "ruleType": "NetworkRule",
+                                "name": "1001-albp",
+                                "ipProtocols": [
+                                    "tcp"
+                                ],
+                                "sourceAddresses": [
+                                    "172.17.1.100-172.17.1.200","172.17.2.100-172.17.2.200","172.17.3.100-172.17.3.200","172.17.4.100-172.17.4.200"
+                                ],
+                                "sourceIpGroups": [],
+                                "destinationAddresses": [
+                                    "192.168.20.15/32"
+                                ],
+                                "destinationIpGroups": [],
+                                "destinationFqdns": [],
+                                "destinationPorts": [
+                                    1433
+                                ]
+                            }
+                        ],
+                        "name": "1001-albp",
+                        "priority": 1010
+                    }
+,
+{
+                        "ruleCollectionType": "FirewallPolicyFilterRuleCollection",
+                        "action": {
+                            "type": "Accept"
+                        },
+                        "rules": [
+                            {
+                                "ruleType": "NetworkRule",
+                                "name": "1002-USgk",
+                                "ipProtocols": [
+                                    "Any"
+                                ],
+                                "sourceAddresses": [
+                                    "172.16.1.0/24","172.16.2.0/24"
+                                ],
+                                "sourceIpGroups": [],
+                                "destinationAddresses": [
+                                    "0.0.0.0/0"
+                                ],
+                                "destinationIpGroups": [],
+                                "destinationFqdns": [],
+                                "destinationPorts": [
+                                    "*"
+                                ]
+                            }
+                        ],
+                        "name": "1002-USgk",
+                        "priority": 1020
+                    }
+] } }
+]}
+```
+
+Hopefully you can find some code here that you can reuse, if converting some text-based configuration into an Azure pattern such as Azure CLI or Azure Resource Manager templates
---
+++ read_fortigate_config.py
@@ -0,0 +1,327 @@
+################################################################################################
+# Reads a fortigate config and exports commands to append the rules to an existing AzFW Policy
+#
+# Example:
+# python ./read_fortigate_config.py ./fortigate_output.txt
+# az network firewall policy rule-collection-group collection rule add --name 1073742000 --source-addresses 10.15.5.20/32 10.15.5.40/32 10.15.5.30/32 10.15.5.50/32 10.5.22.140/32 10.5.22.145/32 10.5.22.150/32 10.5.22.155/32 --destination-addresses 10.10.40.21/32 10.10.40.22/32 --protocols tcp udp --destination-ports 288 33434
+# az network firewall policy rule-collection-group collection rule add --name 1073742041 --source-addresses 10.15.5.40/32 10.15.5.50/32 10.5.22.140/32 10.5.22.150/32 --destination-addresses 10.10.40.21/32 10.10.40.22/32 --protocols tcp udp --destination-ports 22
+#
+#################################################################################################
+
+
+import re
+import sys
+import random
+import string
+import argparse
+from types import prepare_class
+
+
+def netmask_to_cidr(m_netmask):
+  return(sum([ bin(int(bits)).count("1") for bits in m_netmask.split(".") ]))
+
+# Get input arguments
+parser = argparse.ArgumentParser(description='Get the latest flow logs in a storage account')
+parser.add_argument('--file', dest='file_name', action='store',
+                    help='you need to supply a file to analyze')
+parser.add_argument('--format', dest='format', action='store',
+                    default="azcli",
+                    help='output format. Possible values: azcli (default), json')
+parser.add_argument('--verbose', dest='verbose', action='store_true',
+                    default=False,
+                    help='run in verbose mode (default: False)')
+args = parser.parse_args()
+
+
+file_name = args.file_name
+verbose = args.verbose
+
+# Initialize variables
+random.seed()
+rules = []
+new_policy = None  # We start with an empty policy
+supported_protocols = ['tcp', 'udp', 'icmp']
+
+# Process rules line by line
+with open(file_name) as fp:
+    for cnt, line in enumerate(fp):
+        m = re.match('\W+policy\s+(\w+)\s+', line)
+        if m:
+            policy_name = m.groups()[0]
+            # print ("Policy name found:", policy_name)
+            if new_policy:
+                rules.append(new_policy)
+            new_policy={"name": policy_name, "protocols": [], "dstports": [], "action": ""}
+            if verbose:
+                print ("Found new policy", policy_name)
+                input ("Press Enter to continue...")
+        else:
+            # action
+            m = re.match('action\W+(\w+)', line)
+            if m:
+                if m.groups()[0] == "accept":
+                    new_policy["action"] = "Allow"
+                    if verbose:
+                        print ("Adding Allow action")
+                elif m.groups()[0] == "drop":
+                    new_policy["action"] = "Deny"
+                    if verbose:
+                        print ("Adding Deny action")
+                else:
+                    new_policy["action"] = m.groups()[0]
+                    if verbose:
+                        print ("Adding unknown action", m.groups()[0])
+            # comments
+            else:
+                m = re.match('\W+comments\W+\"(.+)\"', line)
+                if m:
+                    new_policy["comments"] = m.groups()[0]
+                    if verbose:
+                        print ("Adding comments:", m.groups()[0])
+                # dstaddr (comes before srcaddr)
+                else:
+                    m = re.match('dstaddr', line)
+                    if m:
+                        new_policy["dstaddr"] = []
+                    # srcaddr
+                    m = re.match('srcaddr', line)
+                    if m:
+                        new_policy["srcaddr"] = []
+                    # If the line is "subnet: 10.0.0.0 255.0.0.0", see whether to put it in srcaddr or dstaddr, depending on whether srcaddr is already a key
+                    m = re.match('\W+subnet\W+([\d\.]+)\W+([\d\.]+)', line)
+                    if m:
+                        # If srcaddr is already a key, it means we are now looking at it
+                        # Otherwise it needs to be dstaddr
+                        cidr = m.groups()[0] + '/' + str(netmask_to_cidr(m.groups()[1]))
+                        if "srcaddr" in new_policy.keys():
+                            new_policy["srcaddr"].append(cidr)
+                            if verbose:
+                                print ("Adding CIDR", cidr, "to source addresses")
+                        else:
+                            new_policy["dstaddr"].append(cidr)
+                            if verbose:
+                                print ("Adding CIDR", cidr, "to destination addresses")
+                    # If the line is just a CIDR (10.0.0.0/8), see whether to put it in srcaddr or dstaddr, depending on whether srcaddr is already a key
+                    m = re.match('(\d+\.\d+\.\d+\.\d+\/\d+)', line)
+                    if m:
+                        # If srcaddr is already a key, it means we are now looking at it
+                        # Otherwise it needs to be dstaddr
+                        cidr = m.groups()[0]
+                        if "srcaddr" in new_policy.keys():
+                            new_policy["srcaddr"].append(cidr)
+                            if verbose:
+                                print ("Adding CIDR", cidr, "to source addresses")
+                        else:
+                            new_policy["dstaddr"].append(cidr)
+                            if verbose:
+                                print ("Adding CIDR", cidr, "to destination addresses")
+                    # The line could be an IP address range
+                    m = re.match('(\d+\.\d+\.\d+\.\d+) - (\d+\.\d+\.\d+\.\d+)', line)
+                    if m:
+                        # If srcaddr is already a key, it means we are now looking at it
+                        # Otherwise it needs to be dstaddr
+                        ip1 = m.groups()[0]
+                        ip2 = m.groups()[1]
+                        ip_range = ip1 + "-" + ip2
+                        if "srcaddr" in new_policy.keys():
+                            new_policy["srcaddr"].append(ip_range)
+                            if verbose:
+                                print ("Adding IP range", ip_range, "to source addresses")
+                        else:
+                            new_policy["dstaddr"].append(ip_range)
+                            if verbose:
+                                print ("Adding IP range", ip_range, "to destination addresses")
+                    # If the line is just a FQDN (10.0.0.0/8), see whether to put it in srcaddr or dstaddr, depending on whether srcaddr is already a key
+                    # Since we have no access to schindler's DNS, I will generate random IP addresses here
+                    m = re.match('^([\w|\.]+)$', line)
+                    if m:
+                        # If srcaddr is already a key, it means we are now looking at it
+                        # Otherwise it needs to be dstaddr
+                        fqdn = m.groups()[0]
+                        cidr = str(random.randint(1,255)) + "." + str(random.randint(1,255)) + "." + str(random.randint(1,255)) + "." + str(random.randint(1,255)) + "/32"
+                        if "srcaddr" in new_policy.keys():
+                            new_policy["srcaddr"].append(cidr)
+                            if verbose:
+                                print ("Adding CIDR", cidr, "to source addresses instead of FQDN", fqdn)
+                        else:
+                            new_policy["dstaddr"].append(cidr)
+                            if verbose:
+                                print ("Adding CIDR", cidr, "to destination addresses instead of FQDN", fqdn)
+                    # Port ranges
+                    m = re.match('\W+(\w+)\-portrange\W+(\d+)', line)
+                    if m:
+                        protocol=m.groups()[0]
+                        port=m.groups()[1]
+                        if not protocol in new_policy["protocols"]:
+                            new_policy["protocols"].append(protocol)
+                            if verbose:
+                                print ("Adding protocol", protocol)
+                        if not port in new_policy["dstports"] and int(port) > 0:
+                            new_policy["dstports"].append(port)
+                            if verbose:
+                                print ("Adding port", port)
+                    # Protocol xyz (normally ICMP or IP)
+                    m = re.match('protocol\W+(\w+)', line)
+                    if m:
+                        protocol=m.groups()[0].lower()
+                        if protocol in supported_protocols:
+                            if not protocol in new_policy["protocols"]:
+                                new_policy["protocols"].append(protocol)
+                                if verbose:
+                                    print ("Adding protocol", protocol)
+                        elif protocol == "ip":
+                            new_policy["protocols"] = ['Any']
+                            new_policy["dstports"] = ["'*'"]
+                            if verbose:
+                                print ("Adding protocol ANY and all ports")
+
+                    # tcp|udp port
+                    m = re.match('(tcp|udp)\W+(\w+)', line)
+                    if m:
+                        protocol=m.groups()[0]
+                        port=m.groups()[1]
+                        if protocol in supported_protocols:
+                            if not protocol in new_policy["protocols"]:
+                                new_policy["protocols"].append(protocol)
+                                if verbose:
+                                    print ("Adding protocol", protocol)
+                            if not port in new_policy["dstports"] and int(port) > 0:
+                                new_policy["dstports"].append(port)
+                                if verbose:
+                                    print ("Adding port", port)
+
+# Add the last found policy
+if new_policy:
+    rules.append(new_policy)
+
+# Az CLI init commands to create an AzFWPolicy, a collection group and a collection
+if args.format == "azcli":
+    rg = 'fortinet'
+    location = 'westeurope'
+    azfw_policy_name = 'mypolicy'
+    azfw_collection_group = 'fortinet'
+    print ("az group create -n {rg} -l {location}".format(rg=rg, location=location))
+    print ("az network firewall policy create -n {azfw_policy_name} -g {rg}".format(azfw_policy_name=azfw_policy_name, rg=rg))
+    print ("az network firewall policy rule-collection-group create -n {azfw_collection_group} --policy-name {azfw_policy_name} -g {rg} --priority 1000".format(azfw_collection_group=azfw_collection_group, azfw_policy_name=azfw_policy_name, rg=rg))
+
+    # Process generated rules
+    priority = 1000
+    for rule in rules:
+        priority += 10
+        # If the only protocol is ICMP, open up all ports
+        if rule["protocols"] == ['icmp']:
+            rule["dstports"] = ["'*'"]
+        # Add a random suffix to the policy name to make sure it is unique
+        random_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+        rule["name"] = rule["name"] + "-" + random_suffix
+        # Send the AzCLI command to stdout
+        print("az network firewall policy rule-collection-group collection add-filter-collection --rule-type NetworkRule -o none", \
+            "-g {rg} --rcg-name {rcg_name} --policy-name {azfw_policy_name} --action {action} --collection-priority {priority} --name {name} --rule-name {name} --source-addresses {srcip} --destination-addresses {dstip} --ip-protocols {prot} --destination-ports {dstports}" \
+                .format(name=rule["name"], srcip=' '.join(rule["srcaddr"]), dstip=' '.join(rule["dstaddr"]), dstports=' '.join(rule["dstports"]), prot=' '.join(rule["protocols"]), action=new_policy["action"], azfw_policy_name=azfw_policy_name, priority=str(priority), rg=rg, rcg_name=azfw_collection_group))
+
+elif args.format == "json":
+    rg = 'fortinet'
+    location = 'westeurope'
+    azfw_policy_name = 'jsonpolicy'
+    azfw_collection_group = 'fortinet'
+    template_header = """{
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": { },
+    "variables": {
+        "location": "[resourceGroup().location]"
+    },
+    "resources": ["""
+    template_footer="]}"
+    policy_resource="""        {{
+            "type": "Microsoft.Network/firewallPolicies",
+            "apiVersion": "2020-11-01",
+            "name": "{azfw_policy_name}",
+            "location": "[variables('location')]",
+            "properties": {{
+                "sku": {{
+                    "tier": "Standard"
+                }},
+                "threatIntelMode": "Alert"
+            }}
+        }},"""
+    rcg_header = """        {{
+            "type": "Microsoft.Network/firewallPolicies/ruleCollectionGroups",
+            "apiVersion": "2020-11-01",
+            "name": "{azfw_policy_name}/{rcg_name}",
+            "location": "[variables('location')]",
+            "dependsOn": [
+                "[resourceId('Microsoft.Network/firewallPolicies', '{azfw_policy_name}'))]"
+            ],
+            "properties": {{
+                "priority": 1000,
+                "ruleCollections": ["""
+    rc = """{{
+                        "ruleCollectionType": "FirewallPolicyFilterRuleCollection",
+                        "action": {{
+                            "type": "{action}"
+                        }},
+                        "rules": [
+                            {{
+                                "ruleType": "NetworkRule",
+                                "name": "{name}",
+                                "ipProtocols": [
+                                    {protocols}
+                                ],
+                                "sourceAddresses": [
+                                    {srcips}
+                                ],
+                                "sourceIpGroups": [],
+                                "destinationAddresses": [
+                                    {dstips}
+                                ],
+                                "destinationIpGroups": [],
+                                "destinationFqdns": [],
+                                "destinationPorts": [
+                                    {dstports}
+                                ]
+                            }}
+                        ],
+                        "name": "{name}",
+                        "priority": {priority}
+                    }}"""
+    rcg_footer = "] } }"
+
+    print (template_header)
+    print (policy_resource.format(azfw_policy_name=azfw_policy_name))
+    print (rcg_header.format(azfw_policy_name=azfw_policy_name, rcg_name=azfw_collection_group))
+
+    # Go over the rules
+    priority = 1000
+    for rule in rules:
+        # If not the first rule, print a ',' to separate JSON objects
+        if priority != 1000:
+            print(',')
+        priority += 10
+        # If the only protocol is ICMP, open up all ports
+        if rule["protocols"] == ['icmp']:
+            rule["dstports"] = ['"*"']
+        # Replace single quotes with double quotes (in case there is another '*' in the port list)
+        rule['dstports'] = [str(item).replace("'", '"') for item in rule['dstports']]
+        # Add a random suffix to the policy name to make sure it is unique
+        random_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+        rule["name"] = rule["name"] + "-" + random_suffix
+        # If no action was detected, default to 'Accept'
+        if rule["action"] == "":
+            rule["action"] = "Accept"
+        # Send the JSON code to stdout
+        protocols = ['"' + item + '"' for item in rule["protocols"]]
+        srcaddr = ['"' + item + '"' for item in rule["srcaddr"]]
+        dstaddr = ['"' + item + '"' for item in rule["dstaddr"]]
+        try:
+            print(rc.format(name=rule['name'], action=rule["action"], priority=priority, protocols=','.join(protocols), srcips=','.join(srcaddr), dstips=','.join(dstaddr), dstports=','.join(rule["dstports"])))
+            # .format(name=rule["name"], srcip=' '.join(rule["srcaddr"]), dstip=' '.join(rule["dstaddr"]), dstports=' '.join(rule["dstports"]), prot=' '.join(rule["protocols"]), action=new_policy["action"], azfw_policy_name=azfw_policy_name, priority=str(priority), rg=rg, rcg_name=azfw_collection_group))
+        except Exception as e:
+            print ("Error", str(e),"when printing rule", str(rule))
+            pass
+
+    # Close the JSON code
+    print (rcg_footer)
+    print (template_footer)
+else:
+    print ("Format", args.format, "not recognized!")
